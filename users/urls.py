40eb6f8c2ce02ae3dc6d9330a4c43bb8f9c9e42b
rafimuna <rafimuna77@gmail.com>
2026-05-12 08:42:36 +0600

    Add update users urls

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),        # ← name='login'
     path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
+    path('register/', views.register_view, name='register'), # ← name='register'
+    path('profile/', views.profile_view, name='profile'),
 ]
